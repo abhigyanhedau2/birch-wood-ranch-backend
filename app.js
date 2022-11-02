1ef77568b050323deada34429644f3b93f5a46e6
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-11-02 21:08:22 +0530

    Add CORS config

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use(cors());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
     next();
 });
 
