fd9211e7ca4c1ad7160e14b9946980831affb2ae
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-11-16 17:00:20 +0530

    foo

--- models/order-model.js
+++ models/order-model.js
@@ -1,27 +1,5 @@
 const mongoose = require('mongoose');
 
-const getISTTime = () => {
-
-    var currentTime = new Date();
-
-    var currentOffset = currentTime.getTimezoneOffset();
-
-    var ISTOffset = 330;   // IST offset UTC +5:30 
-
-    var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset) * 60000);
-
-    // ISTTime now represents the time in IST coordinates
-
-    var hoursIST = ISTTime.getHours()
-    var minutesIST = ISTTime.getMinutes()
-    var secondsIST = ISTTime.getSeconds()
-
-    const finalISTTime = hoursIST + ":" + minutesIST + ":" + secondsIST;
-
-    return finalISTTime;
-
-};
-
 const orderSchema = new mongoose.Schema({
     // Product and it's description for seller to see and user id 
     product: {
@@ -30,8 +8,8 @@ const orderSchema = new mongoose.Schema({
     },
     orderedOn: {
         type: Date,
-        // default: new Date().toLocaleString()
-        default: getISTTime()
+        default: new Date().toLocaleString()
+        // default: getISTTime()
     },
     totalProductsPrice: {
         type: Number
