60a2cc653741493b437baee7372ef777710dfa2c
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-11-16 16:48:52 +0530

    Set date to toISOString()

--- models/order-model.js
+++ models/order-model.js
@@ -9,7 +9,7 @@ const orderSchema = new mongoose.Schema({
     orderedOn: {
         type: Date,
         // default: new Date().toLocaleString()
-        default: (new Date()).toString()
+        default: (new Date()).toISOString()
     },
     totalProductsPrice: {
         type: Number
