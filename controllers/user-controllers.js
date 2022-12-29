fe489bec69d829c2d58a08ad354da9462f8cb9a8
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-12-29 18:08:17 +0530

    Made changes in token generation using pbkdf2 package

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const nodemailer = require('nodemailer');
-const uuid = require('uuid').v4;
+const pbkdf2 = require("pbkdf2");
 
 const User = require('../models/user-model');
 const UserToken = require('../models/user-token-model');
@@ -12,30 +12,14 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Cart = require('../models/cart-model');
 
-const caeserCipher = (email) => {
+const hashMail = async (toBeHashedString) => {
+    // Generate a salt and then create hash
+    const hashedPassword = pbkdf2
+        .pbkdf2Sync(toBeHashedString, "secret", 10, 32, "sha512")
+        .toString("hex");
 
-    const chars = "abcdefghijklmnopqrstuvwxyz1234567890";
-
-    const getEmailChars = email.split('@')[0];
-
-    let ans = "";
-
-    for (let i = 0; i < getEmailChars.length; i++) {
-        const currentChar = getEmailChars[i];
-
-        if (currentChar === " " || currentChar === "!" || currentChar === "?" || currentChar === ".") {
-            ans += currentChar;
-        } else if (chars.indexOf(currentChar) > 25) {
-            ans += currentChar;
-        } else {
-            const val = chars.indexOf(currentChar);
-            const new_val = (val + 13) % 26;
-            ans += chars[new_val];
-        }
-    }
-
-    return ans;
-
+    // Saving both the dynamic salt and hash in the Database
+    return hashedPassword;
 };
 
 const sendToken = catchAsync(async (req, res, next) => {
@@ -64,11 +48,9 @@ const sendToken = catchAsync(async (req, res, next) => {
         }
     });
 
-    const hashedEmail = caeserCipher(email);
+    const hashedEmail = await hashMail(email);
     const resetToken = hashedEmail;
     const hashedToken = await bcrypt.hash(hashedEmail, 12);
-
-    console.log(hashedEmail);
 
     if (!usertoken) {
         await UserToken.create({
@@ -426,7 +408,7 @@ const sendRecoveryMail = catchAsync(async (req, res, next) => {
         }
     });
 
-    const resetToken = uuid();
+    const resetToken = await hashMail(user.number.toString());
     const hashedToken = await bcrypt.hash(resetToken, 12);
 
     const message = `Hey ${user.name}, \n\nForgot your password?\nWe received a request to reset the password for your Birch Wood Ranch Account.\n\nTo reset your password, enter the following token at the forgot password page - ${resetToken}\n\nHave a nice day!\n\nRegards,\nBirch Wood Ranch by Abhigyan Hedau`;
